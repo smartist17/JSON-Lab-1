12991a13ab74fda3038b0be8400b3d1b635ef2ad
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-04-10 20:56:25 -0400

    Finished lab

--- js/colors.js
+++ js/colors.js
@@ -1,33 +1,35 @@
-var colorsArray : {
-    colors=[
-        {
+var colorsArray :{
+    colors: {
+        "green" :{
+
             "name":"green",
             "red":"64",
             "green":"175",
             "blue":"63",
         },
-        {
+        "yellow" :{
             "name":"yellow",
             "red":"239",
             "green":"183",
             "blue":"62"
         },
-        {
+        "red" :{
             name:"red",
             red:"192",
             green:"46",
             blue:"35"
         }
-        {
+        "blue" :{
             "name":"blue",
             "red":"19",
             "green":"71",
             "blue":"144",
         },
-        {
+        "orange" :{
             "name"="orange",
             "red"="253",
             "green"="92",
             "blue"="48"
         },
-    ]
+    }
+}
